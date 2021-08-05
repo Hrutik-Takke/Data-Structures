0f0c0a80d4026016bedfa2a5986e8e7fc49b832c
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-05 05:22:00 -0700

    Accept a number from user and return the count of ON bits using recursion function.

---
+++ Program170.c
@@ -0,0 +1,61 @@
+
+/////////////////////////////////////////////////////////////////////////////////
+//  
+//  Function Name : SumDigitR
+//  Input: Integer
+//  Output: Integer
+//  Description : Accept a number from user and return the count of ON bits using recursion function.
+//  Date: 05/08/2021
+//  Author: Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountOneI(int iNo)
+{
+    int iMask = 1;
+    int i = 0, iCnt = 0;
+    
+    while(i< 32)
+    {
+        if((iNo & iMask) == iMask)
+        {
+            iCnt++;
+        }
+        i++;
+        iMask = iMask << 1;
+    }
+    return iCnt;
+}
+
+int CountOneR(int iNo)
+{
+    static int iMask = 1;
+    static int i = 0, iCnt = 0;
+    
+    if(i< 32)
+    {
+        if((iNo & iMask) == iMask)
+        {
+            iCnt++;
+        }
+        i++;
+        iMask = iMask << 1;
+        CountOneR(iNo);
+    }
+    return iCnt;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+    iRet = CountOneR(iValue);
+    
+    printf("Number of 1's are : %d\n",iRet);
+    return 0;
+}
